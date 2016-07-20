71f6121f13ed05158956a1ea10ec9ec8b01e0587
akm <akm2000@gmail.com>
2016-07-20 16:35:50 +0900

    2.3  Add starter JS code

---
+++ simple-todos/imports/ui/body.js
@@ -0,0 +1,23 @@
+imports/ui/body.js »
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+import { Template } from 'meteor/templating';
+ 
+import './body.html';
+ 
+Template.body.helpers({
+  tasks: [
+    { text: 'This is task 1' },
+    { text: 'This is task 2' },
+    { text: 'This is task 3' },
+  ],
+});
