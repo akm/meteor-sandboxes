d89b3b12c563b00343682e0be2ce90daec23e941
akm <akm2000@gmail.com>
2016-07-21 12:05:35 +0900

    10.9  Only publish tasks the user is allowed to see

--- simple-todos/imports/api/tasks.js
+++ simple-todos/imports/api/tasks.js
@@ -4,8 +4,14 @@ import { check } from 'meteor/check';
 
 if (Meteor.isServer) {
   // This code only runs on the server
+  // Only publish tasks that are public or belong to the current user
   Meteor.publish('tasks', function tasksPublication() {
-    return Tasks.find();
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId },
+      ],
+    });
   });
 }
 
