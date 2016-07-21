7212b461baf18ab260a8fe28332fec0eedc6765c
akm <akm2000@gmail.com>
2016-07-21 11:48:27 +0900

    10.2  Add publication for tasks

--- simple-todos/imports/api/tasks.js
+++ simple-todos/imports/api/tasks.js
@@ -2,6 +2,13 @@ import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
 
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find();
+  });
+}
+
 export const Tasks = new Mongo.Collection('tasks');
 
 Meteor.methods({
