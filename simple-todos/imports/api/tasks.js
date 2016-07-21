20c9d1b372fd0cf265df5ffb23b1e02a1786d172
akm <akm2000@gmail.com>
2016-07-21 12:06:22 +0900

    Fix indentation

--- simple-todos/imports/api/tasks.js
+++ simple-todos/imports/api/tasks.js
@@ -3,16 +3,16 @@ import { Mongo } from 'meteor/mongo';
 import { check } from 'meteor/check';
 
 if (Meteor.isServer) {
-  // This code only runs on the server
-  // Only publish tasks that are public or belong to the current user
-  Meteor.publish('tasks', function tasksPublication() {
-    return Tasks.find({
-      $or: [
-        { private: { $ne: true } },
-        { owner: this.userId },
-      ],
+    // This code only runs on the server
+    // Only publish tasks that are public or belong to the current user
+    Meteor.publish('tasks', function tasksPublication() {
+        return Tasks.find({
+            $or: [
+                { private: { $ne: true } },
+                { owner: this.userId },
+            ],
+        });
     });
-  });
 }
 
 export const Tasks = new Mongo.Collection('tasks');
