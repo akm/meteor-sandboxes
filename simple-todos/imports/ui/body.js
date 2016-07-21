4ec5d66dea87594d282a3044d1bbc8819ff7cb7a
akm <akm2000@gmail.com>
2016-07-21 10:25:16 +0900

    7.3  Add state dictionary to the body

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -1,10 +1,15 @@
 import { Template } from 'meteor/templating';
- 
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
- 
+
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
     tasks() {
         return Tasks.find({}, { sort: { createdAt: -1 } });
@@ -15,17 +20,17 @@ Template.body.events({
   'submit .new-task'(event) {
     // Prevent default browser form submit
     event.preventDefault();
- 
+
     // Get value from form element
     const target = event.target;
     const text = target.text.value;
- 
+
     // Insert a task into the collection
     Tasks.insert({
       text,
       createdAt: new Date(), // current time
     });
- 
+
     // Clear form
     target.text.value = '';
   },
